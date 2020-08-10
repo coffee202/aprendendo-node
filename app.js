3a622f6f40a7653720797dbe3e233a05aeab01c5
coffee202 <marlonrocha30@gmail.com>
2020-08-09 22:09:47 -0300

    add html

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require ("express")
 const app = express()
+const bodyParser = require ("body-parser")
 
 const port = "3000"
 
+app.use(bodyParser.urlencoded({ extended : true}))
 app.use = port 
+app.use = ("view engine", "esj")
 
-console.log("esta ouvindo a porta ", port)
+app.listen(port, () =>{
+    console.log("esta ouvindo a porta ", port)
+})
+
+
+app.get  ('/' , (req, res) => {
+    res.render("index.ejs")  
+})
+
+app.post("/show", (req, res) =>{
+    console.log( req.body )
+    res.send("feito")    
+})
+
